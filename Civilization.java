1760ff496d81ee785900cb5d21a69bb80c448faf
Rob Firstman <Rob Firstman>
2016-09-02 16:45:59 -0400

    Implemented civilization selection

--- Civilization.java
+++ Civilization.java
@@ -1,12 +1,39 @@
+import java.util.Scanner;
+
 public class Civilization {
     static boolean playing = true;
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        String civ = "";
+
         while(playing) {
             //Your game code here
+            boolean hasChosen = false;
+            String[] civArray = new String[4];
+            civArray[0] = "American";
+            civArray[1] = "Zulu";
+            civArray[2] = "English";
+            civArray[3] = "Chinese";
 
-            //Use this to break out of the game loop. 
+            System.out.println("Welcome to Civilization!"
+                + "\nPlease choose a civilization!");
+            System.out.println("\nCivilizations:\n");
+            for (String str : civArray)
+                System.out.println(str);
+
+            //Loop to determine if civilization choice is valid
+            while (hasChosen == false) {
+                System.out.println("\nEnter your civilization:");
+                civ = scan.nextLine();
+
+                for (int i = 0; i < 4; i++)
+                    if (civ.equalsIgnoreCase(civArray[i]))
+                        hasChosen = true;
+            }
+            System.out.println("You chose the " + civ + " civilization!");
+
+            //Use this to break out of the game loop.
             //Feel free to move it around.
             playing = false;
         }
