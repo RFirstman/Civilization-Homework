09ff3fba54f4cfefc93d964eded67c9cddd06be9
Rob Firstman <Rob Firstman>
2016-09-03 16:43:00 -0400

    Added a method for settling cities and adding per-turn resources

--- Civilization.java
+++ Civilization.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Civilization {
     static boolean playing = true;
     static Scanner scan = new Scanner(System.in);
-    static String[] cityArray = new String[20];
-    static int atttacks = 0, militaryUnits = 0, techPoints = 0, happiness = 10;
-    static double gold = 20.5, resources = 30;
-    static Civilization status = new Civilization();
+    static String[] cityArray = new String[5];
+    static int atttacks = 0, militaryUnits = 0, techPoints = 0;
+    static int happiness = 10, numCities = 0;
+    static double gold = 20, resources = 30;
+    static Civilization civObj = new Civilization();
+    static DecimalFormat decimalFormat = new DecimalFormat("###.0#");
+    static String[] civArray = {"American", "Zulu", "English",
+    "Chinese"};
+    static String[] choiceArray = {"Settle a City", "Demolish a City",
+    "Build Militia", "Research Technology", "Attack Enemy City", "End Turn"};
 
 
     public static void main(String[] args) {
 
         while(playing) {
             //Your game code here
-            String[] civArray = new String[4];
-            civArray[0] = "American";
-            civArray[1] = "Zulu";
-            civArray[2] = "English";
-            civArray[3] = "Chinese";
 
             System.out.println("Welcome to Civilization!");
 
@@ -27,7 +29,8 @@ public class Civilization {
                 + " civilization!");
             System.out.println("\nWhat will your first city's name be?");
             cityArray[0] = scan.nextLine();
-            status.displayStatus();
+            numCities++;
+            civObj.displayStatus();
 
             //Use this to break out of the game loop.
             //Feel free to move it around.
@@ -40,8 +43,8 @@ public class Civilization {
     //Method that displays all resources, properties, and cities
     public static void displayStatus() {
         System.out.println("\nStatus"
-            + "\nResources:\t\t" + resources
-            + "\nGold:\t\t\t" + gold
+            + "\nResources:\t\t" + decimalFormat.format(resources)
+            + "\nGold:\t\t\t" + decimalFormat.format(gold)
             + "\nMilitary Units\t\t" + militaryUnits
             + "\nHappiness\t\t" + happiness
             + "\nTechnology Points\t" + techPoints);
@@ -69,11 +72,42 @@ public class Civilization {
             System.out.println("\nEnter your civilization:");
             civ = scan.nextLine();
 
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < civilizations.length; i++)
                 if (civ.equalsIgnoreCase(civilizations[i]))
                         hasChosen = true;
         }
         return civ;
     }
 
+    //Adds per-turn resources/happiness/gold to user's total
+    public static void nextTurn() {
+        resources++;
+        if (happiness > 20)
+            resources = resources + 5*numCities;
+        gold = gold + 3*numCities;
+        if (happiness % 2 == 0)
+            happiness++;
+        else
+            happiness = happiness - 3;
+    }
+
+    //Settles a new city
+    public static void settleCity() {
+        System.out.println("\nCities:");
+        for (int i = 0; i < cityArray.length; i++)
+            if (cityArray[i] != null)
+                System.out.println("\t" + cityArray[i]);
+        if (numCities < 5) {
+            for (int i = 0; i < 5; i++)
+                if (cityArray[i] != null) {
+                 System.out.println("What would you like to name your city?");
+                 civArray[i] = scan.nextLine();
+                 System.out.println("You have settled " + civArray[i] + "!");
+                }
+            numCities++;
+            gold -= 15.5;
+        }
+        else
+            System.out.println("You have too many cities.");
+    }
 }
