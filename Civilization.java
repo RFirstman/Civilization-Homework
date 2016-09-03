b4c417733f67c08ac537c8f2a260a5a0791de10f
Rob Firstman <Rob Firstman>
2016-09-03 14:11:04 -0400

    Added a method to display status of user's civilization's properties and cities

--- Civilization.java
+++ Civilization.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Civilization {
     static boolean playing = true;
-    Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
+    static String[] cityArray = new String[20];
+    static int atttacks = 0, militaryUnits = 0, techPoints = 0, happiness = 10;
+    static double gold = 20.5, resources = 30;
+    static Civilization status = new Civilization();
+
 
     public static void main(String[] args) {
 
@@ -20,6 +25,9 @@ public class Civilization {
 
             System.out.println("You have chosen the " + userCiv
                 + " civilization!");
+            System.out.println("\nWhat will your first city's name be?");
+            cityArray[0] = scan.nextLine();
+            status.displayStatus();
 
             //Use this to break out of the game loop.
             //Feel free to move it around.
@@ -29,13 +37,28 @@ public class Civilization {
 
     }
 
+    //Method that displays all resources, properties, and cities
+    public static void displayStatus() {
+        System.out.println("\nStatus"
+            + "\nResources:\t\t" + resources
+            + "\nGold:\t\t\t" + gold
+            + "\nMilitary Units\t\t" + militaryUnits
+            + "\nHappiness\t\t" + happiness
+            + "\nTechnology Points\t" + techPoints);
+
+        System.out.println("\nCities:");
+        for (int i = 0; i < cityArray.length; i++)
+            if (cityArray[i] != null)
+                System.out.println("\t" + cityArray[i]);
+    }
+
     //Method that takes a string array of civilization choices
     // and takes user input. Also compares the input with the
     // array to ensure valid input.
     public static String chooseCiv(String[] civilizations) {
         boolean hasChosen = false;
         String civ = "";
-        Scanner scan = new Scanner(System.in);
+        //Scanner scan = new Scanner(System.in);
 
         System.out.println("Please choose a civilization."
             + "\nCivilizations:\n");
@@ -52,4 +75,5 @@ public class Civilization {
         }
         return civ;
     }
+
 }
